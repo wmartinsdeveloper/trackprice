b6e70a8613f382b0847e31139340f868e5503f7b
Wellington Martins <wmartinsdeveloper@gmail.com>
2024-03-25 11:41:07 -0300

    Project Creation

---
+++ UserSecurity/usersecurity/src/main/java/com/agrotech/usersecurity/UsersecurityApplication.java
@@ -0,0 +1,13 @@
+package com.agrotech.usersecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersecurityApplication.class, args);
+	}
+
+}
---
+++ UserSecurity/usersecurity/src/test/java/com/agrotech/usersecurity/UsersecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrotech.usersecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
